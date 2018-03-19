63cec06d20dc2452d1de895f771e27a09ac09cf5
Zhengxu Xia <zxxia@ucla.edu>
2018-03-19 15:31:27 -0700

    Almost done with part3

--- Project5/helper_functions.py
+++ Project5/helper_functions.py
@@ -17,6 +17,9 @@ END_T = [1422835860, 1422837180, 1422838020, 1422838620, 1422840960,
          1422841980, 1422844020, 1422845200, 1422846360]
 
 SCORE_DIFF = [0, 7, 0, 7, 0, -3, -10, -3, 4]
+FOOTBALL_KEYWORDS = ['nfl', 'football', 'sport', 'seahawks', 'patriots',
+                     'superbowl', 'super bowl', 'sports', 'new england',
+                     'seattle']
 
 def get_score_diff(timestamp: float) -> int:
     '''
@@ -32,22 +35,39 @@ def is_massachusetts(location: str) -> bool:
     '''
     Return True if location is in Massachusetts otherwise return false.
     '''
-    return (re.match('.*MA.*', location) is not None or
-            re.match('.*Mass.*', location) is not None)
+    pattern1 = re.compile('.*MA.*')
+    pattern2 = re.compile('.*Mass.*')
+    return (pattern1.match(location) is not None or
+            pattern2.match(location) is not None)
 
 def is_washington(location: str) -> bool:
     '''
     Return True if location is in Washington otherwise return false.
     '''
-    return (re.match('.*WA.*', location) is not None or
-            re.match('.*Wash.*', location) is not None)
+    pattern1 = re.compile('.*WA.*')
+    pattern2 = re.compile('.*Wash.*')
+    return (pattern1.match(location) is not None or
+            pattern2.match(location) is not None)
+
+def is_football_fan(dscrp: str) -> int:
+    '''
+    Determine if user is a football fan from user description.
+    '''
+    if dscrp is None:
+        return 0
+    for keyword in FOOTBALL_KEYWORDS:
+        pattern = re.compile(keyword, re.IGNORECASE)
+        if pattern.search(dscrp, re.IGNORECASE) is not None:
+            return 1
+    return 0
 
 def filter_condition(language: str, timestamp: str, location: str) -> bool:
     '''
     Filtering condition to keep useful tweets.
     '''
     return (language == 'en' and
-            float(timestamp) >= 1422833400 and
+            float(timestamp) >= KICK_OFF_TIME and
+            float(timestamp) <= GAME_OVER_TIME and
             (is_massachusetts(location) or is_washington(location)))
 
 
@@ -85,13 +105,15 @@ def extract_features(tweet: dict) -> dict:
     Extract features from tweet
     '''
     features = dict()
-    features['timestamp'] = float(tweet['citation_date'])
+    features['Timestamp'] = float(tweet['citation_date']) - KICK_OFF_TIME
     location = tweet['tweet']['user']['location']
+    dscrp = tweet['tweet']['user']['description']
+    # follower_cnts = tweet['tweet']['user']['followers_count']
     features['Massachusetts'] = int(is_massachusetts(location))
     features['Washington'] = int(is_washington(location))
     features['Retweet Count'] = tweet['tweet']['retweet_count']
-    #features['Patriots Fan'] =
-    #features['Seahawks Fan'] =
-    features['Score Diff'] = get_score_diff(features['timestamp'])
-
+    features['Tweet Polarity'] = get_tweet_polarity(tweet['tweet']['text'])
+    features['Football Fan'] = is_football_fan(dscrp)
+    features['Score Diff'] = get_score_diff(float(tweet['citation_date']))
+    # features['Followers Count'] =
     return features
